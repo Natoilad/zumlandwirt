e24b624152f7c9454d09c589d5ffcdc1c239a016
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-12-20 21:55:00 +0200

    modal-done

--- assets/js/firstModalWindow.js
+++ assets/js/firstModalWindow.js
@@ -2,6 +2,8 @@ window.onload = function () {
   var modal = document.getElementById('modal');
   var closeButton = document.querySelector('.close-button');
   var modalShownTime = localStorage.getItem('modalShownTime');
+  var resimoBtn = document.querySelector('.resimoBtn');
+  var resimoContainer = document.querySelector('.resimoContainer');
 
   if (modalShownTime === null || modalShownTime < Date.now() - 86400000) {
     modal.style.display = 'block';
@@ -10,6 +12,13 @@ window.onload = function () {
 
   closeButton.addEventListener('click', function () {
     modal.style.display = 'none';
+  });
+  resimoBtn.addEventListener('click', function () {
+    if (resimoContainer.style.display === 'block') {
+      resimoContainer.style.display = 'none';
+    } else {
+      resimoContainer.style.display = 'block';
+    }
   });
 
   window.onclick = function (event) {
