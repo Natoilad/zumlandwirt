4e91c7df485723c675c1bce13469522e6663eff4
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-12-26 03:42:01 +0200

    backup

--- assets/js/notif.js
+++ assets/js/notif.js
@@ -12,7 +12,6 @@ document.addEventListener('DOMContentLoaded', function () {
       stickyNotification.style.opacity = '0.8';
     }
   });
-
   // Моделюємо завантаження через певний час
   setTimeout(function () {
     updateNotification('busy');
@@ -20,11 +19,9 @@ document.addEventListener('DOMContentLoaded', function () {
       updateNotification('full');
     }, 3000); // Затримка для ефекту
   }, 3000); // Затримка для ефекту
-
   function updateNotification(status) {
     stickyNotification.classList.remove('free', 'busy', 'full');
     stickyNotification.classList.add(status);
-
     switch (status) {
       case 'free':
         stickyNotification.innerText = 'Geringe Wartezeit';
@@ -41,3 +38,50 @@ document.addEventListener('DOMContentLoaded', function () {
     updateNotification('free');
   }, 9000);
 });
+// testBtn = document.getElementById('testBtn');
+// function setLoad(status) {
+//   console.log('hello');
+//   const stickyNotification = document.getElementById('stickyNotification');
+//   // const loadingPlaceholder = document.getElementById('loadingPlaceholder');
+//   window.addEventListener('scroll', function () {
+//     // Коли прокручено до кінця сторінки
+//     if (
+//       window.scrollY + window.innerHeight >=
+//       document.documentElement.scrollHeight
+//     ) {
+//       stickyNotification.style.opacity = '0';
+//     } else {
+//       stickyNotification.style.opacity = '0.8';
+//     }
+//   });
+
+// Моделюємо завантаження через певний час
+// setTimeout(function () {
+//   updateNotification('busy');
+//   setTimeout(function () {
+//     updateNotification('full');
+//   }, 3000); // Затримка для ефекту
+// }, 3000); // Затримка для ефекту
+
+// function updateNotification(status) {
+//   stickyNotification.classList.remove('free', 'busy', 'full');
+//   stickyNotification.classList.add(status);
+
+//   switch (status) {
+//     case 'free':
+//       stickyNotification.innerText = 'Geringe Wartezeit';
+//       break;
+//     case 'busy':
+//       stickyNotification.innerText = 'Mittlere Wartezeit';
+//       break;
+//     case 'full':
+//       stickyNotification.innerText = 'Lange Wartezeit';
+//       break;
+//   }
+// }
+// updateNotification(status);
+
+// setTimeout(function () {
+//   updateNotification('free');
+// }, 9000);
+// }
